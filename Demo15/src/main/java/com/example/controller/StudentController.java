399312427c02cd5bacd430e2863f2c9295dff121
HP <vishalmorey1998@gmail.com>
2024-12-12 16:03:00 +0530

    Stringg message

--- Demo15/src/main/java/com/example/controller/StudentController.java
+++ Demo15/src/main/java/com/example/controller/StudentController.java
@@ -5,5 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentController 
 {
+public String hii()
+{    System.out.println("!!");
+     System.out.println("!!");
+	return"Hii saurabh";
+	
+}
+
 
 }
